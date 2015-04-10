ffab8cced48abf78a1a5c2704b70d3cd4612f33f
hbzhang <hbzhang@vt.edu>
2015-04-10 17:28:47 -0400

    change

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -5,7 +5,7 @@ require 'oj'
 
 #total_registrations = 0
 
-Dashing.scheduler.every '25s' do
+Dashing.scheduler.every '90s' do
   #last_valuation = current_valuation
   #current_valuation = rand(100)
 
