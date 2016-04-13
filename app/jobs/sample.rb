9d2d01aa7cde66c4ce9f5433290eb8c2b7c995c8
hbzhang <hbzhang@vt.edu>
2016-04-13 12:52:54 -0400

    change

--- app/jobs/sample.rb
+++ app/jobs/sample.rb
@@ -9,9 +9,9 @@ require 'oj'
   #@stats = HTTParty.get(@STATURL)
   #@statshash  = JSON.parse(@stats.body)
   #total_registrations =  @statshash['stats']['total_registrations']
-  # puts   total_registrations 
-  #Dashing.send_event('totalregisters', { current:total_registrations}) #, last: last_valuation 
-  
+  # puts   total_registrations
+  #Dashing.send_event('totalregisters', { current:total_registrations}) #, last: last_valuation
+
 
 Dashing.scheduler.every '50s' do
  runjob
@@ -28,7 +28,7 @@ def runjob
   #APIKEY = 'qFuWyb9A1pIkykQ'
   #@SERVERURL = 'http://nightwing.recsports.vt.edu:3001/api?api_key=' + APIKEY
 
-  #data = HTTParty.get @SERVERURL 
+  #data = HTTParty.get @SERVERURL
 
   #@STATURL = 'http://nightwing.recsports.vt.edu:3001/api/stats?key=123456'
   @STATURL = 'http://run.recsports.vt.edu/api/stats?key=qFuWyb9A1pIkykQ'
@@ -37,15 +37,18 @@ def runjob
 
   #@statshash = Oj.dump(@stats.body)
 
-  #puts @statshash 
-  total_registrations =  @statshash['stats']['total_registrations']
+  #puts @statshash
+  #total_registrations =  @statshash['stats']['total_registrations']
+
+  total_registrations =  @statshash['stats']['checked_in']
+
   #walkup = @statshash['stats']['walkup']
   #checked_in = @statshash['stats']['checked_in']
   #checked_in_first_day = @statshash['stats']['checked_in_first_day']
   #checked_in_percent = checked_in.fdiv(@total_registrations)*100
   #checked_in_percent = checked_in_percent.round
 
-  puts   total_registrations 
+  puts   total_registrations
 
   last_valuation = total_registrations
   #puts walkup
@@ -53,13 +56,13 @@ def runjob
   #puts checked_in_percent
 
   #json = Oj.load(@statshash)
-  #@statshash = JSON.parse(@stats.stats) 
+  #@statshash = JSON.parse(@stats.stats)
 
-  
-  Dashing.send_event('totalregisters', { current:total_registrations}) #, last: last_valuation 
+
+  Dashing.send_event('totalregisters', { current:total_registrations}) #, last: last_valuation
   #Dashing.send_event('walk', { current: walkup, last: walkup })
   #Dashing.send_event('checkedin',   { value: checked_in })
   #Dashing.send_event('checkedinfirstday',   { current: checked_in_first_day, last: checked_in_first_day })
-  
+
 
 end
